ea2cdaefaa0efcfc874d70918a7b0327805cfa57
Martins Okuonghae <martins.okuonghae@gmail.com>
2015-12-01 21:10:32 +0000

    01-12-2015 Updated Excercise Name

---
+++ Excercise2 -  Login with Password and Username/Excercise2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Excercise2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            txtPassword.PasswordChar ='*';
+            txtPassword.MaxLength = 10;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            try {
+                string myConnection = "datasource = localhost; Port = 3306 ;username = root; password = Martins14$";
+                MySqlConnection myconn = new MySqlConnection(myConnection);
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM database.csharptraining WHERE user_name ='" + this.txtUsername.Text + "' and password  = '" + this.txtPassword.Text + "';", myconn);
+
+                MySqlDataReader myReader;
+                myconn.Open();
+                myReader = cmd.ExecuteReader();
+                int count = 0;
+                while (myReader.Read())
+                {
+                    count = count + 1;
+
+                }
+                if (count == 1)
+                {
+                    MessageBox.Show("Username and password is correct");
+                }
+
+                else if (count > 1)
+                {
+
+                    MessageBox.Show("Duplicate Username and Password ... Access denied");
+                }
+
+                else
+                {
+                    MessageBox.Show("Username and password is not correct.. please try again");
+
+                }
+                txtPassword.Text = "";
+                txtUsername.Text = "";
+            }
+
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Exercise1 - Connect to MySql/Exercise1/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Exercise1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try {
+                string myConnection = "datasource = localhost; port = 3306; username = root; password = Martins14$";
+                MySqlConnection myConn = new MySqlConnection(myConnection);
+                MySqlDataAdapter myDataAdapter = new MySqlDataAdapter();
+                myDataAdapter.SelectCommand = new MySqlCommand("SELECT * FROM database.csharptraining;",myConn);
+                MySqlCommandBuilder CB = new MySqlCommandBuilder(myDataAdapter);
+
+                myConn.Open();
+                DataSet ds = new DataSet();
+                MessageBox.Show("Connected");
+                myConn.Close();
+
+            }
+
+            catch (Exception ex) {
+
+                MessageBox.Show(ex.Message);
+
+            }
+
+
+        }
+    }
+}
